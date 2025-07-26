e321a72f925628a1b664dd63af0f6a841b491ab9
omri-gendler <foromri.g@gmail.com>
2025-07-26 09:59:45 +0300

    Refactor server routes and update package.json scripts

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import { bugService } from './public/services/bug.service.js'
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello there'))
+app.get('/', (req, res) => {
+    bugService.query().then(bugs => res.send(bugs))
+})
 
-app.listen(3030, () => console.log('Server ready at port 3030')) 
+app.get('/api/bug/save', (req, res) => { })
+
+app.get('/api/bug/:bugId', (req, res) => { })
+
+app.get('/api/bug/:bugId/remove', (req, res) => { })
+
+
+const port = 3030
+app.listen(port, () => console.log('Server ready at port 3030')) 
