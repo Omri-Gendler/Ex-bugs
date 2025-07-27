6ba5e695080e4f4731936ac67b8fab6ca404c794
omri-gendler <foromri.g@gmail.com>
2025-07-27 20:52:12 +0300

    feat: Enhance bug API with save, get by ID, and remove functionalities

--- server.js
+++ server.js
@@ -9,12 +9,30 @@ app.get('/api/bug', (req, res) => {
         .then(bugs => res.send(bugs))
 })
 
-app.get('/api/bug/save', (req, res) => { })
+app.get('/api/bug/save', (req, res) => {
+    const bug = {
+        title: req.query.title,
+        description: req.query.description,
+        severity: +req.query.severity,
+        _id: req.query._id
+    }
+    bugService.save(bug)
+        .then(savedBug => res.send(savedBug))
+})
 
-app.get('/api/bug/:bugId', (req, res) => { })
+app.get('/api/bug/:bugId', (req, res) => {
+    const bugId = req.params.bugId
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch(err => res.status(404).send({ error: err }))
+})
 
-app.get('/api/bug/:bugId/remove', (req, res) => { })
-
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const bugId = req.params.bugId
+    bugService.remove(bugId)
+        .then(() => res.send({ message: 'Bug removed successfully' }))
+        .catch(err => res.status(404).send({ error: err }))
+})
 
 const port = 3030
 app.listen(port, () => console.log('Server ready at port 3030')) 
