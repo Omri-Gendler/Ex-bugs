89b1690ce3bd6b5dbdd3b2fd403d9dcf79b84f7b
omri-gendler <foromri.g@gmail.com>
2025-08-03 19:37:56 +0300

    style: Add labels to bugs in bug.json and comment out description and createdAt in BugPreview for cleaner display

--- public/cmps/BugPreview.jsx
+++ public/cmps/BugPreview.jsx
@@ -2,8 +2,8 @@ export function BugPreview({bug}) {
     return <article className="bug-preview">
         <p className="title">{bug.title}</p>
         <p>Severity: <span>{bug.severity}</span></p>
-        <p className="description">{bug.description}</p>
-        <p className="created-at">Created at: {new Date(bug.createdAt).toLocaleDateString()}</p>
+        {/* <p className="description">{bug.description}</p> */}
+        {/* <p className="created-at">Created at: {new Date(bug.createdAt).toLocaleDateString()}</p> */}
         <p className="status">Status: {bug.isDone ? 'Done' : 'In Progress'}</p>
 
     </article>
